8a18af51ac7d88967d2bbc823e555341dcb687d4
Scheila Stihler <scheila.stihler@checkstore.com.br>
2025-04-03 08:36:12 -0300

    deploy backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,5 +29,5 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3033;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
